3e2fec618936f3daf8924c0334a76ecd0411191e
A-PachecoT <andre.pacheco.t@uni.pe>
2026-01-31 17:20:32 -0500

    fix: add base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://cofoundy.github.io",
+  base: "/portfolio-ivan-cordova",
   vite: {
     plugins: [tailwindcss()],
   },
