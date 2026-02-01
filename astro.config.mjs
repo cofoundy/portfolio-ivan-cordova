6b6614c1330b2b1cbd8e81c7f1b2db9e6489d289
A-PachecoT <andre.pacheco.t@uni.pe>
2026-02-01 10:50:07 -0500

    fix: update base path for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cofoundy.github.io",
-  base: "/portfolio-ivan-cordova",
+  site: "https://ivancordova.lat",
+  base: "/",
   vite: {
     plugins: [tailwindcss()],
   },
