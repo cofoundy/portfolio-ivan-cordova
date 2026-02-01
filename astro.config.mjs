ae061587e5fa4415516e704faeb7d8c314365b4a
A-PachecoT <andre.pacheco.t@uni.pe>
2026-02-01 10:52:40 -0500

    fix: remove base path for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ivancordova.lat",
-  base: "/",
   vite: {
     plugins: [tailwindcss()],
   },
